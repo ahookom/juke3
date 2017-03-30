d98535415ca3bd384e948b1b0b328578d9dd370b
Student <jeff.kandel@gmail.com>
2017-03-30 10:25:58 -0400

    Added regex filter for artists

--- browser/react/containers/FilterableArtistsContainer.js
+++ browser/react/containers/FilterableArtistsContainer.js
@@ -7,13 +7,20 @@ export default class FilterableArtistsContainer extends React.Component{
   constructor(props){
     super(props);
     this.state={
+      artists: props.artists,
       currentArtists: props.artists,
       filter: ''
     }
     this.updateHandler=this.updateHandler.bind(this);
   }
   updateHandler(e){
-    console.log(e.target.value);
+    const filter = e.target.value;
+    const re = new RegExp(filter,'i')
+    const filteredArtists = this.state.artists.filter(artist => artist.name.match(re))
+    this.setState({
+      currentArtists: filteredArtists,
+      filter:filter
+    })
   }
 
   render(){
